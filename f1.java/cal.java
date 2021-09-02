d1324a667b8b93ef3165caade1f218a53a0acb38
shivanwa <shivani.wankhede@amdocs.com>
2021-09-02 21:17:53 +0530

    program created

---
+++ f1.java/cal.java
@@ -0,0 +1,22 @@
+class calculator
+{
+int x;
+int y;
+public int add(int x,int y)
+{
+this.x=x;
+this.y=y;
+return x+y;
+}
+public int diff(intx, int y)
+{
+this.x=x;
+this.y=y;
+}
+public static void main(String args[]}
+{
+calculatoc cal=new calculator();
+cal.add(3,5);
+cal.diff(3,5};
+}
+}
